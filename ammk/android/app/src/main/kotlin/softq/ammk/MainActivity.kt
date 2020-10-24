7b415d3b261aacf28ddf45d95c2fe82015291851
mariana394 <a01704671@itesm.mx>
2020-10-23 21:15:44 -0500

    feat:setUp flutter

---
+++ ammk/android/app/src/main/kotlin/softq/ammk/MainActivity.kt
@@ -0,0 +1,6 @@
+package softq.ammk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ammk/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+//the main function is the starting point for all pur Flutter apps
+void main() {
+
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('AMMK'),
+        ),
+      )
+    ),
+  );
+}
